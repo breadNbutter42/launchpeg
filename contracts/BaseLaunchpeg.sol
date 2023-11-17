6836cccbadae34a2d3c8d2abad2163adcd03d1d8
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2023-11-17 03:03:53 -0500

    Update BaseLaunchpeg.sol add stringsupgradeable link

--- contracts/BaseLaunchpeg.sol
+++ contracts/BaseLaunchpeg.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
+
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/common/ERC2981Upgradeable.sol";
